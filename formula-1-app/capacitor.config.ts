e2ce1908e4b0a445b11f34e5617365e7d02c21e6
iorimatt <m.iori.franco@gmail.com>
2021-12-22 14:48:58 +0000

    first commit

---
+++ formula-1-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Formula1 App',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
